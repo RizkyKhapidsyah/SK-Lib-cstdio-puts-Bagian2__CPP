5936bd3540b6d5438fec4f8d1ffa13be54975afa
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-03 21:03:12 +0700

    Initial commit

---
+++ SK-Lib-cstdio-puts-Bagian2__CPP/Source.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <conio.h>
+
+/*
+    Source by Programiz (https://www.programiz.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    char str1[] = "Halo 1";
+    char str2[] = "Halo 2";
+
+    puts(str1);
+    puts(str2);
+
+    _getch();
+    return 0;
+}
